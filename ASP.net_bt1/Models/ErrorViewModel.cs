9efee56f65a122d79f9f4f11fa6ab2b2b8138f20
bartng2 <nam.n1352@gmail.com>
2023-07-14 22:16:42 +0700

    TickerStandard

---
+++ ASP.net_bt1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.net_bt1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
